fbfe74eeef43b1d6a91f83925abedd8a9318ba32
MaestreDniel <dmaestre.empleo@gmail.com>
2023-07-30 19:15:33 +0200

    first commit

---
+++ metal-masters/src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h3 class="text-6xl text-emerald-500 rotate-90">Test Tailwind</h3>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
