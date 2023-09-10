928ae068356a490c50ae277b44dfe7f9c9d88eaa
MaestreDniel <dmaestre.empleo@gmail.com>
2023-09-10 12:00:00 +0200

    Add example to get products from api in HomeView

--- metal-masters/src/views/HomeView.vue
+++ metal-masters/src/views/HomeView.vue
@@ -1,9 +1,38 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+
+// import TheWelcome from '../components/TheWelcome.vue'
+
+const products = ref({
+  data: null
+});
+
+const url = "http://localhost:8000"
+
+async function getProducts() {
+  let productsData = await axios.get(`${url}/api/products`);
+  products.value = productsData;
+  products.data = productsData.data;
+}
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <h2 class="text text-3xl mb-3">Products</h2>
+    <form @submit.prevent="getProducts">
+      <div class="flex flex-col h-12 items-center mx-2">
+        <div>
+          <button class="m-2 px-4 py-2 bg-emerald-500 rounded-md text-white">Products</button>
+        </div>
+      </div>
+      <section>
+        {{ products.data }}
+      </section>
+    </form>
   </main>
 </template>
